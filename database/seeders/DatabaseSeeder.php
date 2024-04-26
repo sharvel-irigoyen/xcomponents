1985ffd5b70baec582ee547988ae3475a7a48f73
Javier <sharvel.irigoyen@gmail.com>
2024-04-26 01:45:43 -0500

    add factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,14 +7,11 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\CustomerItem;
 use App\Models\Item;
-use App\Models\ItemPic;
 use App\Models\Role;
 use App\Models\ShoppingCart;
 use App\Models\ShoppingCartDetail;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Http;
-use Faker\Factory as FakerFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -50,28 +47,24 @@ class DatabaseSeeder extends Seeder
             $this->call(ItemSeeder::class);
         }
 
-        // if (ItemPic::count() === 0) {
-        //     ItemPic::factory(5)->create();
-        //     // ItemPic::factory(600)->create();
-        // }
-        // if (CustomerItem::count() === 0) {
-        //     CustomerItem::factory(40)->create();
-        // }
-        // if (ShoppingCartDetail::count() === 0) {
-        //     ShoppingCartDetail::factory(40)->create();
-        // }
+        if (CustomerItem::count() === 0) {
+            CustomerItem::factory(40)->create();
+        }
+        if (ShoppingCartDetail::count() === 0) {
+            ShoppingCartDetail::factory(40)->create();
+        }
 
-        //actualizacion de los precios en ShoppingCart descontando si el producto pertenece al cliente
-        // $shoppingCarts = ShoppingCart::with('shoppingCartDetails.item')->get();
+        // actualizacion de los precios en ShoppingCart descontando si el producto pertenece al cliente
+        $shoppingCarts = ShoppingCart::with('shoppingCartDetails.item')->get();
 
-        // foreach ($shoppingCarts as $shoppingCart) {
-        //     $totalStorePrice = $shoppingCart->shoppingCartDetails->where('item.owner', 'Tienda')->sum('item.price');
-        //     $totalCustomerPrice = $shoppingCart->shoppingCartDetails->where('item.owner', 'Cliente')->sum('item.price');
+        foreach ($shoppingCarts as $shoppingCart) {
+            $totalStorePrice = $shoppingCart->shoppingCartDetails->where('item.owner', 'Tienda')->sum('item.price');
+            $totalCustomerPrice = $shoppingCart->shoppingCartDetails->where('item.owner', 'Cliente')->sum('item.price');
 
-        //     $totalPrice = $totalStorePrice - $totalCustomerPrice;
-        //     $shoppingCart->update([
-        //         'total_price' => $totalPrice
-        //     ]);
-        // }
+            $totalPrice = $totalStorePrice - $totalCustomerPrice;
+            $shoppingCart->update([
+                'total_price' => $totalPrice
+            ]);
+        }
     }
 }
